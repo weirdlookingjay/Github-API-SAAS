fd6c934ef9d7e9d14441cca278ee82bc4a1deed0
Joe Clark <weirdlookingjay@live.com>
2025-01-06 14:53:53 -0500

    (Feature): Created navigation layouts

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  reactStrictMode: true,
+  // experimental: {
+  //     typedRoutes: true,
+  // }
+};
 
 export default config;
---
+++ src/app/(protected)/app-sidebar.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const items = [
+    {
+        title: "Dasboard",
+        url: "/dashboard",
+        icon: LayoutDashboard
+    },
+    {
+        title: "Q&A",
+        url: "/qa",
+        icon: Bot
+    },
+    {
+        title: "Meetings",
+        url: "/meetings",
+        icon: Presentation
+    },
+    {
+        title: "Billing",
+        url: "/billing",
+        icon: CreditCard
+    },
+]
+
+export function AppSidebar() {
+    const pathname = usePathname();
+
+    return (
+        <Sidebar collapsible="icon" variant="floating">
+            <SidebarHeader>
+                Logo
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map(item => {
+                                return (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <Link href={item.url} className={cn({
+                                                '!bg-primary !text-white': pathname === item.url
+                                            }, 'list-none')}>
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                )
+                            })}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
