b1fde6e7bf9c4ae7cc4a90c16259405c91b3f44c
Joe Clark <weirdlookingjay@live.com>
2025-01-07 09:40:18 -0500

    (Feature): Create Project

---
+++ src/app/(protected)/create/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { api } from "@/trpc/react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+type FormInput = {
+    repoUrl: string;
+    projectName: string;
+    githubToken?: string;
+};
+
+const CreatePage = () => {
+    const { register, handleSubmit, reset } = useForm<FormInput>();
+    const createProject = api.project.createProject.useMutation();
+
+    function onSubmit(data: FormInput) {
+        createProject.mutate({
+            githubUrl: data.repoUrl,
+            name: data.projectName,
+            githubToken: data.githubToken
+        }, {
+            onSuccess: () => {
+                toast.success("Project created successfully");
+                reset();
+            },
+            onError: () => {
+                toast.error("Failed to create project");
+            }
+        });
+        return true;
+    }
+
+    return (
+        <div className="flex h-full items-center justify-center gap-12">
+            <img src="/undraw_github.svg" alt="github" className="h-56 w-auto" />
+            <div>
+                <div>
+                    <h1 className="text-2xl font-semibold">
+                        Link your Github Repository
+                    </h1>
+                    <p className="text-sm text-muted-foreground">
+                        Enter the URL of your repository to link it to Dionysus
+                    </p>
+                </div>
+                <div className="h-4" />
+                <div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Input
+                            {...register("projectName", { required: true })}
+                            placeholder="Project Name"
+                            required
+                        />
+                        <div className="h-2" />
+                        <Input
+                            {...register("repoUrl", { required: true })}
+                            placeholder="Github URL"
+                            required
+                            type="url"
+                        />
+                        <div className="h-2" />
+                        <Input
+                            {...register("githubToken")}
+                            placeholder="Github Token (optional)"
+                        />
+                        <div className="h-4" />
+                        <Button type="submit" disabled={createProject.isPending}>
+                            Create Project
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CreatePage;
---
+++ src/server/api/routers/project.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const projectRouter = createTRPCRouter({
+  createProject: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        githubUrl: z.string(),
+        githubToken: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const project = await ctx.db.project.create({
+        data: {
+          githubUrl: input.githubUrl,
+          name: input.name,
+          userToProjects: {
+            create: {
+              userId: ctx.user.userId!,
+            },
+          },
+        },
+      });
+      return project;
+    }),
+});
